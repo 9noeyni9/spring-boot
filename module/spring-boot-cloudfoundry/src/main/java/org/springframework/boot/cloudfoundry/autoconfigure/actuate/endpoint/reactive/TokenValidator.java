98b4d5e64943bc445d9ff636ced1ab035a7cc451
Phillip Webb <phil.webb@broadcom.com>
2025-10-13 14:09:24 -0700

    Start building against Spring Security 7.0.0-RC1 snapshots
    See gh-47499

--- module/spring-boot-cloudfoundry/src/main/java/org/springframework/boot/cloudfoundry/autoconfigure/actuate/endpoint/reactive/TokenValidator.java
+++ module/spring-boot-cloudfoundry/src/main/java/org/springframework/boot/cloudfoundry/autoconfigure/actuate/endpoint/reactive/TokenValidator.java
@@ -30,9 +30,9 @@ import java.util.concurrent.TimeUnit;
 
 import reactor.core.publisher.Mono;
 
-import org.springframework.boot.cloudfoundry.autoconfigure.actuate.endpoint.CloudFoundryAuthorizationException;
-import org.springframework.boot.cloudfoundry.autoconfigure.actuate.endpoint.CloudFoundryAuthorizationException.Reason;
-import org.springframework.boot.cloudfoundry.autoconfigure.actuate.endpoint.Token;
+import org.springframework.boot.cloudfoundry.actuate.autoconfigure.endpoint.CloudFoundryAuthorizationException;
+import org.springframework.boot.cloudfoundry.actuate.autoconfigure.endpoint.CloudFoundryAuthorizationException.Reason;
+import org.springframework.boot.cloudfoundry.actuate.autoconfigure.endpoint.Token;
 import org.springframework.util.Assert;
 
 /**
@@ -86,7 +86,11 @@ class TokenValidator {
 		return this.securityService.fetchTokenKeys()
 			.doOnSuccess(this::cacheTokenKeys)
 			.filter((tokenKeys) -> tokenKeys.containsKey(keyId))
-			.map((tokenKeys) -> tokenKeys.get(keyId))
+			.map((tokenKeys) -> {
+				String tokenKey = tokenKeys.get(keyId);
+				Assert.state(tokenKey != null, "No token key found for '%s'".formatted(keyId));
+				return tokenKey;
+			})
 			.switchIfEmpty(Mono.error(new CloudFoundryAuthorizationException(Reason.INVALID_KEY_ID,
 					"Key Id present in token header does not match")));
 	}
